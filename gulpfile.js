e3f90425e3bcaf2c31cd4095cdeb802b89ff2b44
ethikz <josh.christensen2@gmail.com>
2014-06-12 21:46:30 -0400

    Changed branch

--- gulpfile.js
+++ gulpfile.js
@@ -119,14 +119,13 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('deploy', function () {
+  gulp.src("build/**/*");
+});
+
 
 // Tasks
 // =======================================================
 
 gulp.task('default', ['watch', 'connect']);
-gulp.task('compile', ['html', 'css', 'js', 'copyImages']);
-
-gulp.task('deploy', function () {
-  gulp.src("build/*")
-  .pipe(plugin.deploy(branch, 'gh-pages'));
-});
+gulp.task('compile', ['html', 'css', 'js', 'copyImages', 'deploy']);
